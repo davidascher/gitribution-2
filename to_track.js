2b1412e00ff0b7e561efe5d3100bb940f50dd81a
David Ascher <da@mozillafoundation.org>
2016-04-04 12:32:04 -0700

    Add rust and browser.html

--- to_track.js
+++ to_track.js
@@ -1,2 +1,2 @@
-exports.orgs = ['mozilla-appmaker', 'mozilla', 'mozilla-b2g', 'mozilla-services', 'mozillafordevelopment'];
+exports.orgs = ['mozilla-appmaker', 'mozilla', 'mozilla-b2g', 'mozilla-services', 'mozillafordevelopment', 'MozillaFoundation'];
 //exports.orgs = ['mozilla-b2g'];
